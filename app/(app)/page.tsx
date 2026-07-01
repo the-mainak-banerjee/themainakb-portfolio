73c4990bbf2e181cd134136f79c43f80426a91e9
Mainak <developer.mainakbanerjee@gmail.com>
2026-07-01 10:10:48 +0530

    style: :lipstick: added animation in mobile nav

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <h2 className="text-3xl font-bold font-mono">Mainak Banerjee&apos;s Portfolio</h2>
+    <div>
+      <div className="flex h-[80vh] items-center justify-center">
+        <h2 className="font-mono text-3xl font-bold">Section 1: Home Page</h2>
+      </div>
+      <div className="flex h-[80vh] items-center justify-center">
+        <h2 className="font-mono text-3xl font-bold">Section 2: About Me</h2>
+      </div>
+      <div className="flex h-[80vh] items-center justify-center">
+        <h2 className="font-mono text-3xl font-bold">Section 3: My Projects</h2>
+      </div>
+      <div className="flex h-[80vh] items-center justify-center">
+        <h2 className="font-mono text-3xl font-bold">Section 4: Contact</h2>
+      </div>
     </div>
   );
 }
